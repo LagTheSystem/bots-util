7d3b01a5cafb32ff8a7af2c51a4a5ab1260bf36d
LagTheSystem <lagthesystem@duck.com>
2026-08-21 01:02:21 -0500

    feat: add registry package wrapping Windows registry API

--- src/go.mod
+++ src/go.mod
@@ -32,6 +32,6 @@ require (
 	github.com/wailsapp/wails/v2 v2.15.0 // indirect
 	golang.org/x/crypto v0.53.0 // indirect
 	golang.org/x/net v0.56.0 // indirect
-	golang.org/x/sys v0.46.0 // indirect
+	golang.org/x/sys v0.47.0 // indirect
 	golang.org/x/text v0.39.0 // indirect
 )
---
+++ src/internal/pkg/registry/registry.go
@@ -0,0 +1,90 @@
+//go:build windows
+
+package registry
+
+import (
+	"fmt"
+
+	"golang.org/x/sys/windows/registry"
+)
+
+const (
+	DWORD = registry.DWORD
+	SZ    = registry.SZ
+)
+
+type Registry interface {
+	SetDWORD(key, name string, value uint32) error
+	SetString(key, name, value string) error
+	Delete(key, name string) error
+	KeyExists(key string) (bool, error)
+	ValueExists(key, name string) (bool, error)
+}
+
+type RegistryImpl struct{}
+
+func New() *RegistryImpl {
+	return &RegistryImpl{}
+}
+
+func (r *RegistryImpl) SetDWORD(key, name string, value uint32) error {
+	k, err := registry.OpenKey(registry.LOCAL_MACHINE, key, registry.SET_VALUE)
+	if err != nil {
+		return fmt.Errorf("registry: open key %s: %w", key, err)
+	}
+	defer k.Close()
+
+	if err := k.SetDWordValue(name, value); err != nil {
+		return fmt.Errorf("registry: set DWORD %s\\%s: %w", key, name, err)
+	}
+	return nil
+}
+
+func (r *RegistryImpl) SetString(key, name, value string) error {
+	k, err := registry.OpenKey(registry.LOCAL_MACHINE, key, registry.SET_VALUE)
+	if err != nil {
+		return fmt.Errorf("registry: open key %s: %w", key, err)
+	}
+	defer k.Close()
+
+	if err := k.SetStringValue(name, value); err != nil {
+		return fmt.Errorf("registry: set string %s\\%s: %w", key, name, err)
+	}
+	return nil
+}
+
+func (r *RegistryImpl) Delete(key, name string) error {
+	k, err := registry.OpenKey(registry.LOCAL_MACHINE, key, registry.SET_VALUE)
+	if err != nil {
+		return fmt.Errorf("registry: open key for delete %s: %w", key, err)
+	}
+	defer k.Close()
+
+	if err := k.DeleteValue(name); err != nil {
+		return fmt.Errorf("registry: delete value %s\\%s: %w", key, name, err)
+	}
+	return nil
+}
+
+func (r *RegistryImpl) KeyExists(key string) (bool, error) {
+	k, err := registry.OpenKey(registry.LOCAL_MACHINE, key, registry.QUERY_VALUE)
+	if err != nil {
+		return false, nil
+	}
+	k.Close()
+	return true, nil
+}
+
+func (r *RegistryImpl) ValueExists(key, name string) (bool, error) {
+	k, err := registry.OpenKey(registry.LOCAL_MACHINE, key, registry.QUERY_VALUE)
+	if err != nil {
+		return false, nil
+	}
+	defer k.Close()
+
+	_, _, err = k.GetValue(name, nil)
+	if err != nil {
+		return false, nil
+	}
+	return true, nil
+}
